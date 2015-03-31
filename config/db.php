c54c25032eb28761382872c00d4f0a930e9bd6ff
githubjeka <et.coder@gmail.com>
2015-03-31 14:26:40 +0300

    Moved to composer module

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=c9',
-    'username' => 'githubjeka',
-    'password' => '',
-    'charset' => 'utf8'
-];
